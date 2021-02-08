9758c7f989565b5279c65bd8ceab4960c2bb7f62
Ckyiu <UnsignedArduino@outlook.com>
2021-02-08 15:06:58 -0500

    Ability to click computer

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 namespace SpriteKind {
     export const Thing = SpriteKind.create()
 }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (sprite_cursor_pointer.overlapsWith(sprite_computer)) {
+        sprites.setDataNumber(sprite_computer, "next_number", randint(0, max_height))
+        sprite_computer.say(convertToText(sprites.readDataNumber(sprite_computer, "next_number")))
+        check_for_magic_number(sprites.readDataNumber(sprite_computer, "next_number"))
+    }
+})
 function animate_main_computer (animation_delay: number) {
     animation.runImageAnimation(
     sprite_computer,
@@ -20,7 +27,7 @@ function make_cursor () {
 spriteutils.createRenderable(0, function (screen2) {
     screen2.fillRect(0, 0, 160, 20, 15)
     images.print(screen2, "Score: " + score, 2, 2, 1)
-    images.print(screen2, "Target: " + target_number, 2, 10, 1)
+    images.print(screen2, "Target: " + magic_number, 2, 10, 1)
     screen2.drawLine(0, 20, 160, 20, 1)
     screen2.fillRect(0, 21, 56, 100, 13)
     screen2.drawLine(56, 20, 56, 120, 1)
@@ -32,23 +39,35 @@ function enable_cursor (en: boolean) {
         controller.moveSprite(sprite_cursor_pointer, 0, 0)
     }
 }
+function check_for_magic_number (got: number) {
+    if (got == magic_number) {
+        effects.confetti.startScreenEffect(200)
+        score += 1
+        local_previous_magic_number = magic_number
+        while (magic_number == local_previous_magic_number) {
+            magic_number = randint(0, max_height)
+        }
+    }
+}
 function make_main_computer () {
     sprite_computer = sprites.create(assets.image`computer_monitor`, SpriteKind.Thing)
     sprite_computer.left = 16
     sprite_computer.y = scene.screenHeight() * 0.5
     animate_main_computer(500)
 }
-let sprite_cursor_pointer: Sprite = null
+let local_previous_magic_number = 0
 let sprite_cursor: Sprite = null
 let sprite_computer: Sprite = null
-let target_number = 0
+let sprite_cursor_pointer: Sprite = null
+let magic_number = 0
+let max_height = 0
 let score = 0
 make_cursor()
 make_main_computer()
 scene.setBackgroundColor(11)
 score = 0
-let max_height = 10
-target_number = randint(0, max_height)
+max_height = 10
+magic_number = randint(0, max_height)
 game.onUpdate(function () {
     sprite_cursor.top = sprite_cursor_pointer.top
     sprite_cursor.left = sprite_cursor_pointer.left
